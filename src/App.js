5e664ba52038ac3796ae4f82d0815d5b606f6df6
André Pinho <andrefgpinho@gmail.com>
2019-09-19 16:38:10 +0100

    added facerecognition component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import Form from './components/Form/Form.js';
 import Rank from './components/Rank/Rank.js';
-
-/*
-function App(){
-  return (
-    <div className="App">
-      <Navigation />
-    </div>
-  );
-}
-
-export default App;               
-*/
-
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
 
 const particleOptions = {
   particles: {
@@ -31,7 +20,51 @@ const particleOptions = {
     }
   }
 
+const app = new Clarifai.App({
+ apiKey: 'd53cde8a0c674a1cb2af1e1b913bcbdf'
+  });
+
 class App extends Component {
+  constructor() {
+      super();
+      this.state = {
+          input: '',
+          imageUrl: '',
+          box:{}
+        }
+    }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      rightCol: width - (clarifaiFace.right_col * width),
+      topRow: clarifaiFace.top_row * height,
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+      }
+  }
+
+  displayFaceBox = (box) => { 
+    this.setState({box: box})
+    }
+
+  onInputChange = (event) => {
+      this.setState({ input: event.target.value });
+    }
+
+  onButtonSubmit = (click) => {
+    this.setState({imageUrl: this.state.input})
+
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)  
+        .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+        .catch(err => console.log(err));
+      
+    }
+
   render()
   {
   return (
@@ -40,7 +73,8 @@ class App extends Component {
       <Navigation />
       <Logo />
       <Rank/>
-      <Form />
+      <Form onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+      <FaceRecognition box={this.state.box} image={this.state.imageUrl} />
     </div>
   );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Form.css'
 
-const Form = () => {
+const Form = ({ onInputChange, onButtonSubmit }) => {
 	return (
 		<div>
 			<p className='f3'>
-				{'Isto permite detectar caras nas fotografias que inserires'}
+				{'Permite detectar caras nas fotografias que inserires'}
 			</p>
 			<div className='center'>
 				<div className='center pa4 br3 shadow-5 form'>
-					<input className = 'f4 pa2 w-65 center' type='text'/>
-					<button className = 'w-35 grow f4 link ph3 pv2 dib white bg-black'>Detectar</button>
+					<input className = 'f4 pa2 w-65 center' type='text' onChange={onInputChange}/>
+					<button className = 'w-35 grow f4 link ph3 pv2 dib white bg-black' onClick={onButtonSubmit} >Detectar</button>
 				</div>
 			</div>
 		</div>
