05a0692d25d5a0d1868e0220999d5c569e9a4636
André Pinho <andrefgpinho@gmail.com>
2019-09-19 16:56:13 +0100

    Updates

---
+++ precache-manifest.b33f0bf2f4358a4adbc4fa77cbcf6cc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e5c13d754ae21440824351b89b2ce5",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "d6c127e02a22be1fe328",
+    "url": "/facerecognition/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "fe656cbad2d4444eb498",
+    "url": "/facerecognition/static/css/main.50b8affa.chunk.css"
+  },
+  {
+    "revision": "d6c127e02a22be1fe328",
+    "url": "/facerecognition/static/js/2.a0a6c0ea.chunk.js"
+  },
+  {
+    "revision": "fe656cbad2d4444eb498",
+    "url": "/facerecognition/static/js/main.bf73d306.chunk.js"
+  },
+  {
+    "revision": "88f977ac93cf736e6d47",
+    "url": "/facerecognition/static/js/runtime~main.79f104c5.js"
+  }
+]);
